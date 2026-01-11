96b30795a4153ccd6b1ed2d128f98b303775743b
MuhammadKhizar7 <muhammadkhizar7@gmail.com>
2026-01-12 00:02:46 +0500

    chore(docs): remove deprecated modules from nuxt.config.ts
    - Removed '@nuxtjs/sitemap' and '@nuxtjs/robots' modules from the config
    - Updated modules array to reflect current usage in documentation config
    - Kept devtools enabled setting unchanged

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/content', 'nuxt-og-image', '@nuxtjs/sitemap', 'nuxt-llms', '../src/module', '@nuxtjs/robots'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/content', 'nuxt-og-image', 'nuxt-llms', '../src/module'],
 
   devtools: {
     enabled: true,
